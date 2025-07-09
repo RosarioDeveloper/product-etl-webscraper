dbd3e6749758423b37b3d89a826ee7e37dd56e08
Rosario Massango <rosariomassango@gmail.com>
2025-07-09 01:27:06 +0100

    Feat: Downloading pages from web

--- extractors/ebay.py
+++ extractors/ebay.py
@@ -5,37 +5,118 @@ from pathlib import Path
 import csv
 import re
 import requests
+import aiohttp
+
+cache = dict()
 
 
 class EbayDataExtractor:
 
     def __init__(self):
-        pass
+        self.cache = dict()
+        self.soup = BeautifulSoup()
 
-    async def init(self):
+    async def load_html(self, filename: str):
+        def read_file():
+            with open(f"downloads/{filename}.html", "r") as file:
+                return file.read()
+
+        try:
+            return await asyncio.to_thread(read_file)
+        except Exception:
+            pass
+
+    async def get_html(self, page: int, from_local: bool = False):
+        print(f"Downloading html page {page}")
+
+        async def __get_html():
+            response = await asyncio.to_thread(
+                lambda: requests.get(
+                    f"https://www.ebay.com/sch/i.html?_nkw=iphone&_sacat=0&_from=R40&_pgn={page}"
+                )
+            )
+
+            if not response.ok:
+                raise Exception("404", "Page not founded")
+
+            return response.text
+
+        def read_file():
+            with open(f"downloads/ebay_products_page_{page}.html", "r") as reader:
+                return reader.read()
+
+        def download(html: str):
+            with open(f"downloads/ebay_products_page_{page}.html") as writer:
+                writer.write(html)
+                writer.flush()
+                return html
+
+        if from_local:
+            return await asyncio.to_thread(read_file)
+
+        html = await self.__get_html()
+        return await asyncio.to_thread(lambda: download())
+
+    async def extract(self):
+        filename = "ebay_products_page"
+        html = await self.get_html(page=1)
+
+        soup = BeautifulSoup(html, "html.parser")
+        pagination = soup.select_one(".pagination__items li:last-child")
+
+        if not pagination:
+            raise Exception("No Paginatinon")
+
+        total_pages = int(pagination.text)
+        if total_pages <= 1:
+            return
+
+        # total_pages = 2
+        for page in range(1, total_pages):
+            page_html = await self.get_html(page + 1)
+
+            if not page_html:
+                continue
+
+            print(page + 1)
+
+        total_pages = 0
+
+    # async load
+
+    def trim(self, text: str):
+        return re.sub(r"\s+", " ", text).strip()
+
+    async def extract_data(self):
         # response = requests.get(
-        #     "https://www.ebay.com/sch/i.html?_nkw=iphone&_sacat=0&_from=R40&_trksid=p4432023.m570.l1313"
+        #     "https://www.ebay.com/sch/i.html?_nkw=iphone&_sacat=0&_from=R40&_pgn=1"
         # )
 
         # if response.status_code != 200:
         #     print("I cannot make the scraper")
         #     return
 
-        def load_file(filename: str):
-            with open(filename, "r") as file:
-                return file.read()
-
         path = Path("output/ebay_extracted_data.csv")
         path.parent.mkdir(parents=True, exist_ok=True)
 
-        html = await asyncio.to_thread(load_file, "iphone_sale _ eBay.html")
+        html = await asyncio.to_thread(self.load_file, "iphone_sale _ eBay.html")
         soup = BeautifulSoup(html, "html.parser")
         products = soup.select("ul.srp-results > li")
 
         # Extract the data
-        with open(path.resolve(), "a", newline="", encoding="utf-8") as file:
+        with open(path.resolve(), "a+", newline="", encoding="utf-8") as file:
             buffer: List[dict] = []
-            writer = csv.DictWriter(file, fieldnames=["name", "price"])
+            writer = csv.DictWriter(
+                file,
+                fieldnames=[
+                    "name",
+                    "price",
+                    "second_price",
+                    "old_price",
+                    "store",
+                    "store_address",
+                ],
+            )
 
             # Create the columns if not exist
             if path.stat().st_size <= 0:
@@ -47,9 +128,14 @@ class EbayDataExtractor:
 
                 if not name or not price:
                     continue
-                
-                name = re.sub(r'\s+', ' ', name.text)
-                buffer.append({"name": name.strip(), "price": price.text})
+
+                # Check if the product already exist on the output
+                name = self.trim(name.text)
+                if name in cache:
+                    continue
+
+                cache[name] = cache.get(name, 0) + 1
+                buffer.append({"name": name, "price": self.trim(price.text)})
 
                 # Generate the output
                 if len(buffer) >= 30:
