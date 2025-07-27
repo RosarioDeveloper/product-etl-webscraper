aaa19f1d9490510ebc6fe9eedf21b025d6bf99ce
Rosario Massango <rosariomassango@gmail.com>
2025-07-27 15:21:17 +0100

    Chore: Ractoring donwload_html_page method to fetch_page

--- extractors/ebay.py
+++ extractors/ebay.py
@@ -1,22 +1,10 @@
-import asyncio
-from dataclasses import dataclass
-from types import CoroutineType, NoneType
-from typing import Any, Callable, Coroutine, List, Literal, Optional
 from bs4 import BeautifulSoup
 from pathlib import Path
 import csv
-import requests
-import aiohttp
-from typing import AsyncGenerator, get_type_hints
+from typing import AsyncGenerator
+from psycopg import AsyncConnection
 
-
-cache = dict()
-
-
-@dataclass
-class StreamResult:
-    status: Optional[int] = 200
-    output_file: Optional[str] = None
+from web_scraper import WebScraper
 
 
 class EbayDataExtractor:
@@ -26,24 +14,9 @@ class EbayDataExtractor:
         self.soup = BeautifulSoup()
         self.output_path = Path() / "output"
 
-        # Create the initial download folder
-        # path = Path("downloads")
-
         # Create the initial folders
         (Path() / "downloads").mkdir(exist_ok=True)
         self.output_path.mkdir(exist_ok=True)
-
-    async def pipeline_stream(self):
-        # Run stages in pipelibe mode
-        async def pipeline(*funcs: Callable):
-            result = None
-            for func in funcs:
-                is_coroutine = get_type_hints(func).get("return") is NoneType
-                result = await func(result) if is_coroutine else func(result)
-
-            return result
-
-        await pipeline(self.extract_stage, self.transform_stage, self.load_stage)
 
     async def extract_stage(self, stream=None) -> AsyncGenerator[Path | None]:
 
@@ -67,32 +40,23 @@ class EbayDataExtractor:
 
                 yield clear_text(f"{name_tag.text}:{price_tag.text}")
 
-        # Get the total pages available to scraper
-        page_html = await self.download_html_page(1, from_local=True)
-        soup = BeautifulSoup(page_html, "html.parser")
+        async with WebScraper() as scraper:
+            output_file = self.output_path / "extrated_data.txt"
 
-        pagination = soup.select_one(".pagination__items li:last-child")
-        if not pagination:
-            yield None
-            return
+            with open(output_file, "w+") as file:
+                print("Extrating Data")
+                for i in range(1, 3):
+                    result = await scraper.fetch_page(
+                        f"https://www.ebay.com/sch/i.html?_nkw=iphone&_sacat=0&_from=R40&_pgn={i}"
+                    )
 
-        total_pages = int(pagination.text)
+                    if "error" in result:
+                        continue
 
-        # Start Extraction
-        output_file = self.output_path / "extrated_data.txt"
-        with open(output_file, "w+") as file:
-            print(f"Extracting Data Page ")
-
-            for i in range(1, 3):
-                html = (
-                    page_html
-                    if i == 1
-                    else await self.download_html_page(i, from_local=True)
-                )
-
-                async for product in extract_product(html):
-                    if product:
-                        file.write(f"{product}\n")
+                    html = result.get("html")
+                    async for product in extract_product(str(html)):
+                        if product:
+                            file.write(f"{product}\n")
 
             yield output_file
 
@@ -120,64 +84,31 @@ class EbayDataExtractor:
 
         yield transformed_data_dir
 
-    async def load_stage(self, stream: AsyncGenerator[StreamResult]) -> None:
-        async for data in stream:
-            print(f"Loading Data")
+    async def load_stage(self, stream: AsyncGenerator[Path]) -> None:
 
-    async def read_file(self, dir: str):
-        with open(dir, "r") as reader:
-            return reader.read()
+        async with await AsyncConnection.connect(
+            "dbname=data_extract_db user=admin password=admin host=localhost port=5433"
+        ) as conn:
+            print("Loading Data")
+            async with conn.cursor() as query:
 
-    async def write_file(self, dir: str, content: str):
-        with open(dir, "a") as writer:
-            writer.write(content)
-            writer.flush()
-
-    async def download_html_page(
-        self,
-        page: int,
-        url: str = "",
-        from_local: bool = False,
-    ) -> str:
-        # Download html page
-        async def download_page(url: str):
-            # headers = {
-            #     "Cache-Control": "no-cache",
-            #     "Pragma": "no-cache",
-            #     "Expires": "0",
-            #     "User-Agent": "Mozilla/5.0",
-            # }
-
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url) as response:
-                    return await response.text()
-
-        # Controll concurrency
-        html = ""
-        async with asyncio.Semaphore(10):
-            try:
-                filename = f"downloads/ebay_products_page_{page}.html"
-                if from_local:
-                    return await self.read_file(filename)
-
-                html = await download_page(
-                    f"https://www.ebay.com/sch/i.html?_nkw=iphone&_sacat=0&_from=R40&_pgn={page}"
+                # Drop table if exist
+                await query.execute(
+                    """ create table if exists products (
+                    id    serial primary key,
+                    name  varchar(255),
+                    price float
+                    ) """
                 )
 
-                await self.write_file(filename, content=html)
+                dir = await stream.asend(None)
 
-            except Exception as e:
-                html = ""
+                # Load data to database
+                with open(dir, "r", newline="") as file:
+                    reader = csv.DictReader(file)
 
-            return html
-
-    async def extract_product(self, html: BeautifulSoup):
-        products = html.select("ul.srp-results > li")
-
-        name = html.find("div", class_="s-item__title")
-        price = html.select_one(".s-item__price")
-
-        # async for i, product in enumerate(products):
-        #     pass
-
-        # return {"name": name, "price": price}
+                    async with query.copy(
+                        "COPY products (name, price) FROM STDIN"
+                    ) as copy:
+                        for row in reader:
+                            await copy.write_row([row["name"], row["price"]])
--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from python-webscraper!")
+import asyncio
+from types import NoneType
+from typing import Callable, get_type_hints
+
+from extractors.ebay import EbayDataExtractor
+from web_scraper import WebScraper
+
+
+# Run stages in pipelibe mode
+async def pipeline(*funcs: Callable):
+    result = None
+
+    for func in funcs:
+        is_coroutine = get_type_hints(func).get("return") is NoneType
+        result = await func(result) if is_coroutine else func(result)
+
+    return result
+
+
+async def main():
+    ebay_extractor = EbayDataExtractor()
+
+    await pipeline(
+        ebay_extractor.extract_stage,
+        ebay_extractor.transform_stage,
+        ebay_extractor.load_stage,
+    )
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
